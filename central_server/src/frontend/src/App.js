bb7f116528e2942121ae0878c81ec9dcfe5ab97b
Abigail Amelia Amethyst <abigail.amethyst@protonmail.com>
2025-03-03 15:47:21 +0000

    Setting up React app and HTML Canvas
    Setting up initial files and code for frontend as well as integrating the HTML Canvas for use with React.

---
+++ central_server/src/frontend/src/App.js
@@ -0,0 +1,15 @@
+import Canvas from "./Canvas/Canvas";
+
+function App() {
+
+  const draw = (context, count) => {
+      context.clearRect(0,0,context.canvas.width,context.canvas.height)
+      context.font = "50px Arial"
+      const delta = count % 800
+      context.fillText("Hello World",10 + delta, 80)
+  }
+
+  return <Canvas draw={draw} width="800" height="500"/>
+}
+
+export default App;
