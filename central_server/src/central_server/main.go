fc08b1fa1a995ce82d73e9f092ed9d2eb19983d6
ZadeAbhishek <zabhidoc@gmail.com>
2025-03-08 09:56:04 +0000

    Added Curl

--- central_server/src/central_server/main.go
+++ central_server/src/central_server/main.go
@@ -18,6 +18,7 @@ import (
 var (
 	client        *mongo.Client
 	mongoURI      = os.Getenv("MONGO_URI")
+	db            = make(map[string]string)
 	kafkaProducer *kafka.Producer
 	kafkaConsumer *kafka.Consumer
 	broadcastTopic = "central_to_chunk_broadcast"
@@ -44,13 +45,66 @@ func connectToDB(ctx context.Context) (*mongo.Client, error) {
 // setupRouter initializes HTTP routes.
 func setupRouter() *gin.Engine {
 	r := gin.Default()
+    
+	r.GET("/dbconn", func(c *gin.Context) {
+		// Ping the database
+		err := client.Ping(context.Background(), nil)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Unable to connect to database"})
+		}
 
+		c.String(http.StatusOK, "Able to connect to DB")
+	})
 	// Health check
 	r.GET("/ping", func(c *gin.Context) {
 		c.String(http.StatusOK, "pong")
 	})
 
+	r.GET("/user/:name", func(c *gin.Context) {
+		user := c.Params.ByName("name")
+		value, ok := db[user]
+		if ok {
+			c.JSON(http.StatusOK, gin.H{"user": user, "value": value})
+		} else {
+			c.JSON(http.StatusOK, gin.H{"user": user, "status": "no value"})
+		}
+	})
+    
+	// Authorized group (uses gin.BasicAuth() middleware)
+	// Same than:
+	// authorized := r.Group("/")
+	// authorized.Use(gin.BasicAuth(gin.Credentials{
+	//	  "foo":  "bar",
+	//	  "manu": "123",
+	//}))
+	authorized := r.Group("/", gin.BasicAuth(gin.Accounts{
+		"foo":  "bar", // user:foo password:bar
+		"manu": "123", // user:manu password:123
+	}))
+
+	authorized.POST("admin", func(c *gin.Context) {
+		user := c.MustGet(gin.AuthUserKey).(string)
+
+		// Parse JSON
+		var json struct {
+			Value string `json:"value" binding:"required"`
+		}
+
+		if c.Bind(&json) == nil {
+			db[user] = json.Value
+			c.JSON(http.StatusOK, gin.H{"status": "ok"})
+		}
+	})
+
 	// Send a broadcast message to all chunk servers
+	// This is just for tesing purpose but we can use this function iternally to communicate
+	/* 
+	
+	curl -X POST http://<central_server_host>:8080/broadcast \
+     -H "Content-Type: application/json" \
+     -d '{"message": "Your broadcast message"}'
+	
+	*/
 	r.POST("/broadcast", func(c *gin.Context) {
 		var req struct {
 			Message string `json:"message" binding:"required"`
@@ -71,6 +125,12 @@ func setupRouter() *gin.Engine {
 	})
 
 	// Send a message to a specific chunk server
+	/* 
+	curl -X POST http://<central_server_host>:8080/send/<chunk_id> \
+     -H "Content-Type: application/json" \
+     -d '{"message": "Your message"}'
+	
+	*/
 	r.POST("/send/:chunk_id", func(c *gin.Context) {
 		chunkID := c.Param("chunk_id")
 		chunkTopic := fmt.Sprintf("central_to_chunk_%s", chunkID)
@@ -94,6 +154,12 @@ func setupRouter() *gin.Engine {
 	})
 
 	// Register a new chunk server
+	/* 
+	curl -X POST http://<central_server_host>:8080/register_chunk \
+     -H "Content-Type: application/json" \
+     -d '{"chunk_id": "chunk_server_123"}'
+	
+	*/
 	r.POST("/register_chunk", func(c *gin.Context) {
 		var req struct {
 			ChunkID string `json:"chunk_id" binding:"required"`
@@ -118,8 +184,11 @@ func setupRouter() *gin.Engine {
 
 		c.JSON(http.StatusOK, gin.H{"status": "Chunk registered"})
 	})
-
+ 
 	// List all registered chunk servers
+	/*
+	curl -X GET http://<central_server_host>:8080/list_chunks
+	*/
 	r.GET("/list_chunks", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"chunk_servers": chunkServers})
 	})
